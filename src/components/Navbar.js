fa41acbf9b673c6b98cd72e9dbf789daa10d6e99
Josephine Hedman <josephine.hedman@gmail.com>
2021-05-20 11:25:10 +0200

    updated styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,20 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <ul className="navbar__list">
-                <li className="navbar__list-item">
-                    <Link to="/" className="navbar__links" activeClassName="active__link">Home</Link>
-                </li>
-                <li className="navbar__list-item">
-                    <Link to="/about" className="navbar__links" activeClassName="active__link">About</Link>
-                </li>
-                <li className="navbar__list-item">
-                    <Link to="/portfolio" className="navbar__links" activeClassName="active__link">Portfolio</Link>  
-                </li>
+                <div>
+                    <p className="name__box">JOSEPHINE <br/> HEDMAN</p>
+                </div>
+                <div className="navbar__link-container">
+                    <li className="navbar__list-item">
+                        <Link to="/" className="navbar__links" activeClassName="active__link">Home</Link>
+                    </li>
+                    <li className="navbar__list-item">
+                        <Link to="/about" className="navbar__links" activeClassName="active__link">About</Link>
+                    </li>
+                    <li className="navbar__list-item">
+                        <Link to="/portfolio" className="navbar__links" activeClassName="active__link">Portfolio</Link>  
+                    </li>
+                </div>
             </ul>
         </nav>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,35 +4,19 @@ import Layout from '../components/Layout'
 import Img from "gatsby-image"
 import '../styles/index.css'
 
-export default function Home({ data }) {
+export default function Home() {
   return (
     <Layout>
       <section className="hompage__container">
-        <article className="homepage__left-box">
           <h2 className="hompage__heading">
-            JOSEPHINE <br/>HEDMAN
+           HELLO!
           </h2>
           <div className="connect__container">
+            <p> Connect with me: </p>
             <img href='https://www.iconsdb.com/icons/preview/black/linkedin-5-xxl.png' alt='linkedin logo'/>
           </div>
-        </article>
-        <article className="homepage__right-box">
-          {/* <Img fluid={data.allContentfulAsset.nodes[0].fluid} alt="Josephine Hedman" className="img-profile"></Img>  */}
-        </article>
     </section>
     </Layout>
     )
 }
 
-export const imgQuery =
-graphql`
-query MyQuery {
-  allContentfulAsset(filter: {id: {eq: "e5acc676-407e-5018-9c50-74b4bc76a436"}}) {
-    nodes {
-      fluid {
-        ...GatsbyContentfulFluid
-      }
-    }
-  }
-}
-`

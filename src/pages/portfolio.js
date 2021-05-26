abceccb8f5c85385c8d05fb3998a4a7d2579b7b3
Josephine Hedman <josephine.hedman@gmail.com>
2021-05-26 17:34:55 +0200

    started styling portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Img from "gatsby-image"
+import '../styles/portfolio.css'
 import { graphql } from 'gatsby'
 
 const Portfolio = ({ data }) => {
@@ -8,20 +8,25 @@ const nodes = data.allContentfulProject.edges;
 
     return (
         <Layout>
-            <div>
+            <section className="portfolio__container">
+                <article className="portfolio__content">
+                    <h2 className="portfolio__heading">My Projects</h2>
                 <ul>
                     {nodes.map(node => {
                         return (
                             <li> 
                                 <div>
-                                {node.node.body.body}
+                                    <p className="portfolio__paragraph">
+                                        {node.node.body.body}
+                                    </p>
                                 {/* <Img fluid={node.node.image.fluid} alt={node.node.image.fluid.title}></Img>  */}
                                 </div>
                             </li>
                         )
                     })}
                 </ul>
-            </div>
+                </article>
+            </section>
         </Layout>
     )
 }

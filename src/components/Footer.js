7a3aeef42763c4422b2aaea114afd15a8f59ad0c
Josephine Hedman <77280354+josephinehedman@users.noreply.github.com>
2021-08-31 11:50:50 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,12 @@ const Footer = () => {
         <footer className="footer">
             <h2 className="footer__heading"> Connect with me </h2>
             <div className="footer__logos">
-              <a href="https://www.linkedin.com/in/josephine-hedman/" target="_blank" className><img src={linkedin} alt='linkedin logo' className='connect-logo'/></a>
-              <a href="https://github.com/josephinehedman"target="_blank"><img src={github} alt='github logo' className='connect-logo'/></a>
+              <a href="https://www.linkedin.com/in/josephine-hedman/" target="_blank" className>
+                <img src={linkedin} alt='linkedin logo' className='connect-logo'/>
+              </a>
+              <a href="https://github.com/josephinehedman"target="_blank">
+                  <img src={github} alt='github logo' className='connect-logo'/>
+              </a>
             </div>
         </footer>
     )
